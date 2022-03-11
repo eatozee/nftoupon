644cc1df958ba8b5d2f649f3986dfbc3d2717cc0
amirgal <amit.s.mirgal@gmail.com>
2022-03-10 19:38:22 -0500

    added Creator and Arbiter component

---
+++ src/Arbiter.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Arbiter = () => {
+  return <div>{'Arbiter'}</div>;
+};
---
+++ src/Creator.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  NextUIProvider,
+  createTheme,
+  Button,
+  Card,
+  Divider,
+  Input,
+  Row,
+  Spacer,
+  Text,
+  Textarea,
+  Switch,
+} from '@nextui-org/react';
+import { Send } from 'react-iconly';
+import { BiSun, BiMoon } from 'react-icons/bi';
+
+const myDarkTheme = createTheme({
+  type: 'dark',
+});
+
+const lightTheme = createTheme({
+  type: 'light',
+});
+
+export const Creator = () => {
+  const [isDark, setIsDark] = React.useState(false);
+
+  return (
+    <NextUIProvider theme={isDark ? myDarkTheme : lightTheme}>
+      <Card css={{ mw: '330px' }}>
+        <Card.Header>
+          <Row justify="space-between">
+            <Text b size={18}>
+              NFToupon Generator
+            </Text>
+            <Switch
+              onChange={(event) => setIsDark(!event.target.checked)}
+              color="error"
+              checked={true}
+              size="sm"
+              iconOn={<BiSun />}
+              iconOff={<BiMoon />}
+            />
+          </Row>
+        </Card.Header>
+        <Divider />
+        <Card.Body css={{ py: '$12' }}>
+          <Spacer y={0.5} />
+          <Input size="md" clearable underlined labelPlaceholder="Title" />
+          <Spacer y={1.5} />
+          <Textarea underlined labelPlaceholder="Description" />
+          <Spacer y={1.5} />
+          <Input clearable underlined type="file" />
+        </Card.Body>
+        <Divider />
+        <Card.Footer>
+          <Row justify="flex-end">
+            <Button auto color="error" iconRight={<Send set="bulk" />}>
+              NFToupon
+            </Button>
+          </Row>
+        </Card.Footer>
+      </Card>
+    </NextUIProvider>
+  );
+};
