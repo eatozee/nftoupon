d8a582e7263dc0f633c0fb8c64b2de3c63427ee6
amirgal <amit.s.mirgal@gmail.com>
2022-03-10 19:43:10 -0500

    fixed unit test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { Default as Creator } from '../stories/Creator.stories';
 
 describe('Thing', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Creator />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
