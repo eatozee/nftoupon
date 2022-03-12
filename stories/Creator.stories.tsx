9c3b192747fd114584be41bab3082c93418d3ee7
amirgal <amit.s.mirgal@gmail.com>
2022-03-12 13:20:16 -0500

    added props to Creator story comopnent

--- stories/Creator.stories.tsx
+++ stories/Creator.stories.tsx
@@ -3,23 +3,21 @@ import { Meta, Story } from '@storybook/react';
 import { Creator } from '../src';
 
 const meta: Meta = {
-  title: 'Welcome',
+  title: 'Creator',
   component: Creator,
-  argTypes: {
-    children: {
-      control: {
-        type: 'text',
-      },
-    },
-  },
-  parameters: {
-    controls: { expanded: true },
-  },
 };
 
 export default meta;
 
-const Template: Story = (args) => <Creator {...args} />;
+const Template: Story = (args) => (
+  <Creator
+    xummConfig={{
+      XUMM_APIKEY: '',
+      XUMM_APISECRET: '',
+    }}
+    {...args}
+  />
+);
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
