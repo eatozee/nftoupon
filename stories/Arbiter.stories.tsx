6c41961e9de157611bf76879868315340a8e03c0
amirgal <amit.s.mirgal@gmail.com>
2022-04-04 10:53:05 -0400

    fixed storybook

--- stories/Arbiter.stories.tsx
+++ stories/Arbiter.stories.tsx
@@ -19,7 +19,7 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story = (args) => <Arbiter {...args} />;
+const Template: Story = (args) => <Arbiter NFToupon_Key="" {...args} />;
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
--- stories/Creator.stories.tsx
+++ stories/Creator.stories.tsx
@@ -9,15 +9,7 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story = (args) => (
-  <Creator
-    xummConfig={{
-      XUMM_APIKEY: '',
-      XUMM_APISECRET: '',
-    }}
-    {...args}
-  />
-);
+const Template: Story = (args) => <Creator NFToupon_Key="" {...args} />;
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
