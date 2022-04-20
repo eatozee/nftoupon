2e7dfe3c6722213e45911a790cf462ec53dfe0d2
Devan Patel <devanpatel1008@gmail.com>
2022-04-20 18:38:34 -0400

    Created comments for toast

--- packages/nftoupon/src/common/constants.ts
+++ packages/nftoupon/src/common/constants.ts
@@ -1,5 +1,11 @@
 const CONNECT_WALLET_URL = `https://eatozee-crypto.app/api/nftoupon/connect`;
+const ERROR_IN_API = 'Something went wrong. Please try again later.';
+const FAIL_SIGN = 'Transaction failed to sign.';
+const EXPIRED = 'Transaction expired.';
 
 export {
     CONNECT_WALLET_URL,
+    ERROR_IN_API,
+    FAIL_SIGN,
+    EXPIRED
 }
