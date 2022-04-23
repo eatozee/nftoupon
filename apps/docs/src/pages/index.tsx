e8fe2d590d28a42e4409eeb783ba5dfcdf6b5725
dhruvin2803 <59930781+dhruvin2803@users.noreply.github.com>
2022-04-23 16:15:02 -0400

    Refactored Arbiter Section

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,9 +1,10 @@
-import { Creator } from "nftoupon";
+import { Arbiter, Creator } from "nftoupon";
 
 export default function Docs() {
 	return (
 		<div style={{ marginTop: "20px" }}>
-			<Creator NFToupon_Key="your-api-key" />
+			{/* <Creator NFToupon_Key="36feff68-ae2a-46a1-9719-20a3fd5e633d" /> */}
+			<Arbiter NFToupon_Key="36feff68-ae2a-46a1-9719-20a3fd5e633d" />
 		</div>
 	);
 }
--- packages/nftoupon/src/common/constants.ts
+++ packages/nftoupon/src/common/constants.ts
@@ -1,5 +1,24 @@
 const CONNECT_WALLET_URL = `https://eatozee-crypto.app/api/nftoupon/connect`;
+const ACCEPT_OFFER_URL = `https://eatozee-crypto.app/api/nftoupon/offer/accept`;
+const ERROR_IN_API = "Something went wrong. Please try again later.";
+const FAIL_SIGN = "Transaction failed to sign.";
+const EXPIRED = "Transaction expired.";
+const DEFAULT_PREVIEW_IMG_URL =
+  "https://djfteveaaqqdylrqovkj.supabase.co/storage/v1/object/public/beta-eatozee-web/nft-free.webp";
+const CARGO_URL = `https://eatozee-crypto.app/api/nftoupon/cargo`;
+const NFTOKEN_CREATOR_OFFER_URL = `https://eatozee-crypto.app/api/nftoupon/merchant/update`;
+const GET_ARBITER_DETAILS_URL = `https://eatozee-crypto.app/api/nftoupon/merchant`;
+const ARBITER_BUY_URL = `https://eatozee-crypto.app/api/nftoupon/offer/buy`;
 
 export {
-    CONNECT_WALLET_URL,
-}
+  CONNECT_WALLET_URL,
+  ERROR_IN_API,
+  FAIL_SIGN,
+  EXPIRED,
+  DEFAULT_PREVIEW_IMG_URL,
+  ACCEPT_OFFER_URL,
+  CARGO_URL,
+  NFTOKEN_CREATOR_OFFER_URL,
+  GET_ARBITER_DETAILS_URL,
+  ARBITER_BUY_URL,
+};
