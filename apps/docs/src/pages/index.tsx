35f721ead80083bcdd3db7be732fdd0d5036ca55
dhruvin2803 <59930781+dhruvin2803@users.noreply.github.com>
2022-04-25 23:01:04 -0400

    Removed API key

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -3,8 +3,7 @@ import { Arbiter, Creator } from "nftoupon";
 export default function Docs() {
 	return (
 		<div style={{ marginTop: "20px" }}>
-			{/* <Creator NFToupon_Key="36feff68-ae2a-46a1-9719-20a3fd5e633d" /> */}
-			<Arbiter NFToupon_Key="36feff68-ae2a-46a1-9719-20a3fd5e633d" />
+			<Creator NFToupon_Key="your-api-key" />
 		</div>
 	);
 }
