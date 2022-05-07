752e2eba2d3ec1e5813a7121a45e0557f49cd70b
amirgal <amit.s.mirgal@gmail.com>
2022-05-07 14:00:30 -0400

    added upload file functionality

--- packages/nftoupon/src/common/constants.ts
+++ packages/nftoupon/src/common/constants.ts
@@ -1,5 +1,7 @@
 const CONNECT_WALLET_URL = `https://eatozee-crypto.app/api/nftoupon/connect`;
+const PREVIEW_IMAGE_URL = `https://djfteveaaqqdylrqovkj.supabase.co/storage/v1/object/public/beta-eatozee-web/nft-free.webp`;
 
 export {
     CONNECT_WALLET_URL,
+    PREVIEW_IMAGE_URL
 }
