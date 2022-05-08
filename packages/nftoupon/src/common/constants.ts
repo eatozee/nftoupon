294a605427fd01cffff81a00184763561937a642
dhruvin2803 <59930781+dhruvin2803@users.noreply.github.com>
2022-05-08 12:52:09 -0400

    Added some constants so that I can use it in Arbiter

--- packages/nftoupon/src/common/constants.ts
+++ packages/nftoupon/src/common/constants.ts
@@ -1,7 +1,24 @@
+//Common Constants for arbiter and creator
 const CONNECT_WALLET_URL = `https://eatozee-crypto.app/api/nftoupon/connect`;
 const PREVIEW_IMAGE_URL = `https://djfteveaaqqdylrqovkj.supabase.co/storage/v1/object/public/beta-eatozee-web/nft-free.webp`;
+const ERROR_IN_API = "Something went wrong. Please try again later.";
+const FAIL_SIGN = "Transaction failed to sign.";
+const EXPIRED = "Transaction expired.";
+const CARGO_URL = `https://eatozee-crypto.app/api/nftoupon/cargo`;
+
+//Constants for Arbiter
+const NFTOKEN_CREATOR_OFFER_URL = `https://eatozee-crypto.app/api/nftoupon/merchant/update`;
+const GET_ARBITER_DETAILS_URL = `https://eatozee-crypto.app/api/nftoupon/merchant`;
+const ARBITER_BUY_URL = `https://eatozee-crypto.app/api/nftoupon/offer/buy`;
 
 export {
     CONNECT_WALLET_URL,
-    PREVIEW_IMAGE_URL
+    PREVIEW_IMAGE_URL,
+    ERROR_IN_API,
+    FAIL_SIGN,
+    EXPIRED,
+    NFTOKEN_CREATOR_OFFER_URL,
+    GET_ARBITER_DETAILS_URL,
+    ARBITER_BUY_URL,
+    CARGO_URL
 }
