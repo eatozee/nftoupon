0a7f4202eda4c1340215bb2c5d1ae20c02388a84
amirgal <amit.s.mirgal@gmail.com>
2022-06-08 07:51:33 -0400

    added index file

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { Creator } from "nftoupon";
 
 export default function Docs() {
-  return (
-    <div style={{ marginTop: "20px" }}>
-      <Creator NFToupon_Key="36feff68-ae2a-46a1-9719-20a3fd5e633d" />
-    </div>
-  );
+	return (
+		<div style={{ marginTop: "20px" }}>
+			<Creator NFToupon_Key="<Your-api-key>" />
+		</div>
+	);
 }
