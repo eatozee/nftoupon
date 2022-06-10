c3259ae7a946f3be384237e6341f2125d241fd16
amirgal <amit.s.mirgal@gmail.com>
2022-06-09 21:08:06 -0400

    removed the api key

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,10 +1,9 @@
-import { Arbiter, Creator } from "nftoupon";
+import { Creator } from "nftoupon";
 
 export default function Docs() {
 	return (
-		<div style={{ marginTop: "20px", display: "flex"}}>
-			<Arbiter NFToupon_Key="36feff68-ae2a-46a1-9719-20a3fd5e633d" />
-			<Creator NFToupon_Key="36feff68-ae2a-46a1-9719-20a3fd5e633d" />
+		<div style={{ marginTop: "20px", display: "flex" }}>
+			<Creator NFToupon_Key="Your API key" />
 		</div>
 	);
 }
